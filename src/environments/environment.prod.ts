46429cde104c93ce9b05b2dc45fd30cdba3619d3
sudhanshug <me@sudhanshug.com>
2019-05-01 22:02:01 +0530

    removes boilerplate code and adds env for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAbFYec4T2sFYdCMxjjs0sjG3Y51ExaHQM',
+    authDomain: 'zolostays-app.firebaseapp.com',
+    databaseURL: 'https://zolostays-app.firebaseio.com',
+    projectId: 'zolostays-app',
+    storageBucket: 'zolostays-app.appspot.com',
+    messagingSenderId: '585478039491'
+  }
 };
